76ef999c9ea5c42f8580f23d3d2fcfb49119b7a3
CallumCheng <callumcheng@outlook.com>
2022-08-24 16:09:19 +0100

    start page in input box

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts";
 
 export default function Login() {
     const [inputValue, setInputValue] = useState("");
-    const {setUser} = useAuth()
+    const {setUser} = useAuth();
+    const inputRef = useRef();
 
     const goTo = useNavigate();
 
     function handleInput(e) {
-        setInputValue(e.target.value);
+        setInputValue(e.target.value); 
     }
 
     async function handleSubmit(e) {
@@ -17,14 +18,19 @@ export default function Login() {
         
         //set global variable for user
         setUser(inputValue)
-
-        goTo("/");
+        goTo("/");  //sends to homepage
         
     }
 
+    useEffect(() => {
+        inputRef.current.focus() //starts the page in the input box
+    },[])
+
+ 
+
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" onChange={handleInput} value={inputValue} placeholder="username" autoComplete="off"/>
+            <input ref={inputRef} type="text" onChange={handleInput} value={inputValue} placeholder="username" autoComplete="off"/>
             <br />
             <input type="submit" />
         </form>
